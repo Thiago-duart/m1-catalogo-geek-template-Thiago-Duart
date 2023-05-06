17a17012b31ff21e4e98becf42d2fa0b965b2de4
ThiagoDbraga <thiagomrkt@gmail.com>
2023-05-06 16:42:12 -0300

    teste-criando-elemento

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,83 @@
 const productsArray = [
-  // Produto 1
   {
     image: "./assets/img/paintings/quadro-c3po.jpg",
     name: "Robot",
     price: "R$250,00",
     type: "Paintings",
   },
-  // Produto 2
   {
     image: "./assets/img/actions/dragonball-action-figure.jpg",
     name: "Goku Super Saiyajin",
     price: "R$500,00",
     type: "Action Figures",
   },
-  // Produto 3 (adicione os produtos restantes abaixo)
+  {
+    image: "./assets/img/paintings/quadro-controle.jpg",
+    name: "Controle Playstation",
+    price: 'R$99,00',
+    type: "Paintings"
+  },
+  {
+    image: "./assets/img/paintings/quadro-mario.jpg",
+    name: 'Mario',
+    price: 'R$275,00',
+    type: 'Paintings'
+  },
+  {
+    image: "./assets/img/paintings/quadro-pacman.jpg",
+    name: 'PacMan',
+    price: 'R$160,00',
+    type: 'Paintings'
+  },
+  {
+    image:  "./assets/img/paintings/quadro-relogio.jpg",
+    name: 'Smart Watch',
+    price: 'R$120,00',
+    type: 'Paintings'
+  },
+  {
+    image: "./assets/img/actions/godzilla-action-figure.jpg",
+    name: 'Godzilla',
+    price: 'R$700,00',
+    type: 'Action Figures'
+  },
+  {
+    image: "./assets/img/actions/groot-action-figure.jpg",
+    name: 'Groot',
+    price: 'R$950,00',
+    type: 'Action Figures'
+  },
+  {
+    image: "./assets/img/actions/ironman-action-figure.jpg",
+    name: 'Homem de Ferro',
+    price: 'R$1030,00',
+    type: 'Action Figures'
+  },
+  {
+    image: "./assets/img/actions/starwars-action-figure.jpg",
+    name: 'Sr Yoda',
+    price: 'R$870,00',
+    type: 'Action Figures'
+  },
+  {
+    image: "./assets/img/actions/wolverine-action-figure.jpg",
+    name: 'Wolverine',
+    price: 'R$510,00',
+    type: 'Action Figures'
+  }
 ];
+
+function addProduct (){
+  const createLi = document.createElement('li')
+  const createImg = document.createElement('img')
+  const createH1 = document.createElement('h1')
+  const createp = document.createElement('p')
+  const select = document.querySelector('ul')
+  createImg.setAttribute('src', './assets/img/actions/wolverine-action-figure.jpg')
+  createH1.innerText = "teste"
+  createp.innerText = 'R$ 123teste'
+  createLi.append(createImg,createH1,createp)
+  select.append(createLi)
+}
+addProduct()
+console.log(window.document.querySelector())
